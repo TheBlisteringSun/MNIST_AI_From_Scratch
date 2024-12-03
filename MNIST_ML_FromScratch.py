658e464344d43264e22995597046730354b883ea
Andy Han <167143235+TheBlisteringSun@users.noreply.github.com>
2024-12-02 19:42:01 -0500

    Fixed some bugs and implemented real-time learning graph
    Found an error in the q3 formula: was sum(a3)**2 instead of (sum(a3) + kj)**2
    
    Also created graphs of the cost in real-time

--- MNIST_ML_FromScratch.py
+++ MNIST_ML_FromScratch.py
@@ -1,8 +1,10 @@
 import random
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 import numpy as np
 from Read_MNIST_Dataset import MnistDataloader
 from os.path import join
+import time
 
 #
 # Set file paths based on added MNIST Datasets
@@ -39,7 +41,7 @@ def Initializing():
         w3 = wbz["w3"]
         b3 = wbz["b3"]
     else:
-        # Initializing weights & biasis
+        # Initializing weights & biases
         w1 = np.random.rand(20, 784)
         b1 = np.random.rand(20)
         w2 = np.random.rand(20, 20)
@@ -67,104 +69,131 @@ def dReLu(array, a):
     return np.array(out)
 
 
-def LeekyReLu(array):
+def LeakyReLu(array):
     output = np.copy(array)
     output[array < 0] *= 0.01
     return output
 
 
-def dLeekyReLu(array):
+def dLeakyReLu(array):
     derivatives = np.ones(array.shape)
     derivatives[array < 0] = 0.01
     return derivatives
 
 
-def FeedForwards_BackwardsPropogation(training_indexes, w1, b1, w2, b2, w3, b3):
+def FeedForwards_BackwardsPropogation(training_indexes, w1, b1, w2, b2, w3, b3, gradient):
     cost = np.zeros((10,))
-    # Feed Forward
-    for index in training_indexes:
-        input_layer = x_test[index]
-        a1 = LeekyReLu(np.dot(w1, input_layer) + b1)
-        a2 = LeekyReLu(np.dot(w2, a1) + b2)
-        a3 = LeekyReLu(np.dot(w3, a2) + b3)
-        P = a3 / sum(a3)  # Percentage
 
-    # Back Prop
-    # q#, v#, p# are the partial derivatives of a#, w#, b# respectively
-        expected_output = np.zeros((10))
+    for index in training_indexes:
+        # Feed Forward
+        input_layer = x_test[index]
+        expected_output = np.zeros(10)
         expected_output[y_test[index]] = 1  # One-Hot Encoding
 
-        dP = 2 * (P - expected_output)
-        q3 = np.array(list(map(lambda x: (sum(a3) - x) / sum(a3) ** 2, a3))) * dP  # dC/da3
+        a1 = LeakyReLu(np.dot(w1, input_layer) + b1)
+        a2 = LeakyReLu(np.dot(w2, a1) + b2)
+        a3 = LeakyReLu(np.dot(w3, a2) + b3)
+        P = a3 / sum(a3)  # Percentage
+        C = (P - expected_output) ** 2
 
-        v3, p3 = np.array([i * a2 for i in q3]) * dLeekyReLu(a3).reshape((a3.shape[0], 1)), np.copy(q3) * dLeekyReLu(a3)
-        q2 = np.dot(np.transpose(w3), q3 * dLeekyReLu(a3))
+        # Back Prop
+        # q#, v#, p# are the partial derivatives of C/a#, C/w#, C/b# respectively
 
-        v2, p2 = np.array([i * a1 for i in q2]) * dLeekyReLu(a2).reshape((a2.shape[0], 1)), np.copy(q2) * dLeekyReLu(a2)
-        q1 = np.dot(np.transpose(w2), q2 * dLeekyReLu(a2))
+        dCP = 2 * (P - expected_output)
+        k = [sum(a3) - aj for aj in a3]
+        q3 = np.array(list(map(lambda kj: kj / (sum(a3) + kj) ** 2, k))) * dCP  # dC/da3
+        v3 = np.array([i * a2 for i in q3]) * dLeakyReLu(a3).reshape((a3.shape[0], 1))
+        p3 = np.copy(q3) * dLeakyReLu(a3)
 
-        v1, p1 = np.array([i * x_test[index] for i in q1]) * dLeekyReLu(a1).reshape((a1.shape[0], 1)), np.copy(q1) * dLeekyReLu(a1)
+        q2 = np.dot(np.transpose(w3), q3 * dLeakyReLu(a3))
+        v2 = np.array([i * a1 for i in q2]) * dLeakyReLu(a2).reshape((a2.shape[0], 1))
+        p2 = np.copy(q2) * dLeakyReLu(a2)
 
+        q1 = np.dot(np.transpose(w2), q2 * dLeakyReLu(a2))
+        v1 = np.array([i * x_test[index] for i in q1]) * dLeakyReLu(a1).reshape((a1.shape[0], 1))
+        p1 = np.copy(q1) * dLeakyReLu(a1)
 
-        np.add(gw1, v1, out=gw1)
-        np.add(gb1, p1, out=gb1)
-        np.add(gw2, v2, out=gw2)
-        np.add(gb2, p2, out=gb2)
-        np.add(gw3, v3, out=gw3)
-        np.add(gb3, p3, out=gb3)
+        for w_or_b, g in zip([v1, p1, v2, p2, v3, p3], gradient):
+            g += w_or_b
 
-        squarer = lambda x: x ** 2
-        cost += squarer(P - expected_output)
-    return sum(cost)
+        cost += C
+    return sum(cost), P
 
 
 def Shuffling_Dataset(datalen, chunk_size):  # Returns chunks of random indexes
     chunks = []
-    indexs = [i for i in range(datalen)]
-    random.shuffle(indexs)
+    indexes = [i for i in range(datalen)]
+    random.shuffle(indexes)
     for i in range(datalen // chunk_size):
-        chunks.append(indexs[chunk_size*i:chunk_size*(i+1)])
+        chunks.append(indexes[chunk_size * i:chunk_size * (i + 1)])
     if datalen % chunk_size != 0:
-        chunks.append(indexs[-datalen % chunk_size:])
+        chunks.append(indexes[-datalen % chunk_size:])
     return chunks
 
 
-def Gradient_Decent(l_r, datalen, w1, b1, w2, b2, w3, b3, gw1, gb1, gw2, gb2, gw3, gb3):
-    nw1 = np.add(gw1 * l_r / datalen, w1)
-    nb1 = np.add(gb1 * l_r / datalen, b1)
-    nw2 = np.add(gw2 * l_r / datalen, w2)
-    nb2 = np.add(gb2 * l_r / datalen, b2)
-    nw3 = np.add(gw3 * l_r / datalen, w3)
-    nb3 = np.add(gb3 * l_r / datalen, b3)
+def Gradient_Decent(l_r, datalen, w1, b1, w2, b2, w3, b3, gradient):
+    updated_weights_and_biases = []
+    for w_or_b, g in zip([w1, b1, w2, b2, w3, b3], gradient):
+        updated_weights_and_biases.append(w_or_b - l_r * g / datalen) # dividing by datalen makes sure the gradient is averaged
 
-    return nw1, nb1, nw2, nb2, nw3, nb3
+    return updated_weights_and_biases
 
-
-learning_rate = -3e2
-num_epochs = 10
+# Having a learning rate < -1 means you might overshoot on max batch_size
+# You should have a lower learning rate for smaller batch_sizes
+# Having smaller batch_sizes speeds up the learning
+# -3e2 good for 10000, -6.9e-1 for 1000
+learning_rate = 5.0e-2  # 2.088e-4
+num_epochs = 100
 batch_size = 10000
 costs = []
-w1, b1, w2, b2, w3, b3 = Initializing()  # Initializing weights and biases
-for epoch in range(num_epochs):
-    training_sets = Shuffling_Dataset(len(y_test), batch_size)
-    for i in range(len(y_test)//batch_size):
-        # Initializing the gradient
-        gw1, gb1, gw2, gb2, gw3, gb3 = Initializing_Gradient(
-            [w1.shape, b1.shape, w2.shape, b2.shape, w3.shape, b3.shape])
+plt.ion()
 
-        cost = FeedForwards_BackwardsPropogation(training_sets[i], w1, b1, w2, b2, w3, b3)
-        w1, b1, w2, b2, w3, b3 = Gradient_Decent(learning_rate, batch_size, w1, b1, w2, b2, w3, b3, gw1,
-                                                 gb1, gw2, gb2, gw3, gb3)
-        costs.append(cost)
+fig, ax = plt.subplots()
+ln, = ax.plot(costs)
 
-    if epoch % max(num_epochs // 20, 1) == 0:  # Progress Percentage
-        print(f"Progress: {round(epoch / num_epochs, 2) * 100}%")
+def Minimize_Cost(learning_rate, num_epochs, batch_size):
+    global lowest_cost_params
+    global current_params
+    global history
+    global costs
+    w1, b1, w2, b2, w3, b3 = Initializing()  # Initializing weights and biases
+    for epoch in range(num_epochs):
+        training_sets = Shuffling_Dataset(len(y_test), batch_size)
+        for i in range(len(y_test) // batch_size):
+            # Initializing the gradient
+            gradient = Initializing_Gradient(
+                [w1.shape, b1.shape, w2.shape, b2.shape, w3.shape, b3.shape])
 
-print(w1, b1, w2, b2, w3, b3)
-print(f"{costs[0] = }, {costs[-1] = }, {min(costs) = }")
-plt.plot(costs)
+            cost, history = FeedForwards_BackwardsPropogation(training_sets[i], w1, b1, w2, b2, w3, b3, gradient)
+            w1, b1, w2, b2, w3, b3 = Gradient_Decent(learning_rate, batch_size, w1, b1, w2, b2, w3, b3, gradient)
+            cost /= batch_size
+            costs.append(cost)
+
+            if cost == min(costs):
+                lowest_cost_params = w1, b1, w2, b2, w3, b3
+
+        if epoch % max(num_epochs // 20, 1) == 0:  # Progress Percentage
+            print(f"Progress: {round(epoch / num_epochs, 2) * 100}%")
+            plt.cla()
+            plt.plot(costs)
+            plt.show()
+    current_params = w1, b1, w2, b2, w3, b3
+
+plt.plot([cost / batch_size for cost in costs if cost < batch_size])
 plt.show()
+# gen_costs = Minimize_Cost(learning_rate, num_epochs, batch_size)
+# last_cost = next(gen_costs)
+# animation = FuncAnimation(plt.gcf(), func=update_plot, interval=1000,save_count=10)
+
+Minimize_Cost(learning_rate, num_epochs, batch_size)
+
+print(history)
+print(f"{costs[0] = }, {costs[-1] = }, {min(costs) = }")
 
 if input("Save? [Y]\n").upper() == "Y":
+    if input("Save the weights & biases with the lowest cost? [L] (Otherwise saves current state)\n").upper() == "L":
+        w1, b1, w2, b2, w3, b3 = lowest_cost_params
+    else:
+        w1, b1, w2, b2, w3, b3 = current_params
     np.savez(join(input("Input path to where to save the weights and biases:\n"), "Weights_and_Biases"), w1=w1, b1=b1,
              w2=w2, b2=b2, w3=w3, b3=b3)
